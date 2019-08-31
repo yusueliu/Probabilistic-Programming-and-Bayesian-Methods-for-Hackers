ace3bb784d6c75a93bdac70dd5fdd07a39536b72
Sue Liu <sue.liu@mudano.com>
2019-08-31 07:59:33 +0100

    Python scripts extracted by myself

--- ipynb/utils.py
+++ ipynb/utils.py
@@ -1,6 +1,4 @@
-import os
-import numpy as np
-import pandas as pd
+from __future__ import absolute_import, division, print_function
 
 #@markdown This sets the warning status (default is `ignore`, since this notebook runs correctly)
 warning_status = "ignore" #@param ["ignore", "always", "module", "once", "default", "error"]
@@ -10,21 +8,25 @@ with warnings.catch_warnings():
     warnings.filterwarnings(warning_status, category=DeprecationWarning)
     warnings.filterwarnings(warning_status, category=UserWarning)
 
+import numpy as np
+import os
 #@markdown This sets the styles of the plotting (default is styled like plots from [FiveThirtyeight.com](https://fivethirtyeight.com/))
 matplotlib_style = 'fivethirtyeight' #@param ['fivethirtyeight', 'bmh', 'ggplot', 'seaborn', 'default', 'Solarize_Light2', 'classic', 'dark_background', 'seaborn-colorblind', 'seaborn-notebook']
+import matplotlib as mpl
 import matplotlib.pyplot as plt; plt.style.use(matplotlib_style)
 import matplotlib.axes as axes;
 from matplotlib.patches import Ellipse
 from mpl_toolkits.mplot3d import Axes3D
-import matplotlib as mpl
+#import pandas_datareader.data as web
 
-import pandas_datareader.data as web
 import seaborn as sns; sns.set_context('notebook')
-
 from IPython.core.pylabtools import figsize
+#@markdown This sets the resolution of the plot outputs (`retina` is the highest resolution)
+notebook_screen_res = 'retina' #@param ['retina', 'png', 'jpeg', 'svg', 'pdf']
 
 import tensorflow as tf
 tfe = tf.contrib.eager
+
 # Eager Execution
 #@markdown Check the box below if you want to use [Eager Execution](https://www.tensorflow.org/guide/eager)
 #@markdown Eager execution provides An intuitive interface, Easier debugging, and a control flow comparable to Numpy. You can read more about it on the [Google AI Blog](https://ai.googleblog.com/2017/10/eager-execution-imperative-define-by.html)
@@ -37,12 +39,11 @@ if use_tf_eager:
     except:
         pass
 
-# Tensorflow probability
 import tensorflow_probability as tfp
 tfd = tfp.distributions
 tfb = tfp.bijectors
 
-
+  
 def evaluate(tensors):
     """Evaluates Tensor or EagerTensor to Numpy `ndarray`s.
     Args:
@@ -59,7 +60,6 @@ def evaluate(tensors):
             [t.numpy() if tf.contrib.framework.is_tensor(t) else t
              for t in tf.contrib.framework.nest.flatten(tensors)])
     return sess.run(tensors)
-
 
 class _TFColor(object):
     """Enum of colors used in TF docs."""
@@ -86,8 +86,7 @@ class _TFColor(object):
         ][i % 9]
 TFColor = _TFColor()
 
-
-def session_options(enable_gpu_ram_resizing=True, enable_xla=False):
+def session_options(enable_gpu_ram_resizing=True, enable_xla=True):
     """
     Allowing the notebook to make use of GPUs if they're available.
     
@@ -103,7 +102,7 @@ def session_options(enable_gpu_ram_resizing=True, enable_xla=False):
     if enable_xla:
         # Enable on XLA. https://www.tensorflow.org/performance/xla/.
         config.graph_options.optimizer_options.global_jit_level = (
-            tf.OptimizerOptions.ON_1)
+            tf.compat.v1.OptimizerOptions.ON_1)
     return config
 
 
@@ -112,13 +111,13 @@ def reset_sess(config=None):
     Convenience function to create the TF graph & session or reset them.
     """
     if config is None:
-        config = session_options(enable_gpu_ram_resizing=True, enable_xla=False)
+        config = session_options()
     global sess
     tf.compat.v1.reset_default_graph()
     try:
         sess.close()
     except:
         pass
-    sess = tf.InteractiveSession(config=config)
+    sess = tf.compat.v1.InteractiveSession(config=config)
 
 reset_sess()
